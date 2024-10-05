d8fe3c7f0e4d165fcd26e74491411aae0ef09a5b
xntkrnl <maminbors@gmail.com>
2024-10-05 17:12:08 +0300

    terminalPosition + remove junk

--- CruiserTerminal/CTFunctions.cs
+++ CruiserTerminal/CTFunctions.cs
@@ -11,6 +11,7 @@ namespace CruiserTerminal
         //TerminalScript scale 0.3571936 0.24 1.71 position -0.06 
         public static void Spawn()
         {
+            ///Cruiser Terminal
             var cruiser = GameObject.Find("CompanyCruiser(Clone)");
             var terminal = GameObject.Instantiate(CTPlugin.mainAssetBundle.LoadAsset("CruiserTerminal.prefab") as GameObject);
 
@@ -21,30 +22,16 @@ namespace CruiserTerminal
             CTPlugin.mls.LogInfo("cruiserNO is null = " + (cruiserNO == null));
 
             terminalNO.Spawn();
+
             bool tmp = terminalNO.TrySetParent(cruiserNO);
             CTPlugin.mls.LogInfo("tmp is = " + tmp);
-            //terminal.transform.SetParent(cruiser.transform);
 
             terminal.name = "Cruiser Terminal";
-            terminal.transform.localPosition = new Vector3(1.293f, 0.938f, -3.274f);
-            //terminal.transform.localScale = new Vector3(0.2f, 0.3f, 0.3f);
 
-            //CopyFromTerminal(terminal.transform);
+            ///terminalPosition
+            GameObject terminalPosition = new GameObject("terminalPosition");
+            terminalPosition.transform.parent = cruiser.transform;
+            terminalPosition.transform.localPosition = new Vector3(1.293f, 0.938f, -3.274f);
         }
-        
-        static void CopyFromTerminal(Transform parentObj)
-        {
-            var go = GameObject.Find("Terminal").transform;
-            GameObject canvas;
-
-            foreach (Transform child in go)
-            {
-                if (child.name == "Canvas")
-                {
-                    canvas = GameObject.Instantiate(child.gameObject, parentObj);
-                }
-            }
-        }
-        
     }
 }
